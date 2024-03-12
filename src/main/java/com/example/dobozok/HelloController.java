b026ab0d53215d95741a04ec4a4a52ae0d3dffb4
balhun <balazshunor333@gmail.com>
2024-03-12 13:38:12 +0100

    Kattint

--- src/main/java/com/example/dobozok/HelloController.java
+++ src/main/java/com/example/dobozok/HelloController.java
@@ -13,14 +13,18 @@ public class HelloController {
     public Pane pane;
     public Label feldolgLb;
     public Label letettLb;
-    public ImageView arrow;
+    public int feldolgDb = 0;
+    public int letettDb = 0;
 
+
+    public ImageView arrow;
     public Image boxopen = new Image(getClass().getResourceAsStream("boxopen.png"));
     public Image box = new Image(getClass().getResourceAsStream("box.png"));
     public Image semmi = new Image(getClass().getResourceAsStream("null.png"));
 
     public Label[][] lbT = new Label[10][15];
     public int[][] intT = new int[10][15];
+    public long[][] eletT = new long[10][15];
 
 
     public final int NULL = 0;
@@ -29,6 +33,7 @@ public class HelloController {
 
     public AnimationTimer timer = null;
     public long ido = 0;
+    public long most = 0;
     public int angle = 0;
 
     public void initialize() {
@@ -38,6 +43,7 @@ public class HelloController {
 
                 lbT[y][x] = new Label("");
                 intT[y][x] = NULL;
+                eletT[y][x] = 0;
                 lbT[y][x].setGraphic(new ImageView(semmi));
 
                 lbT[y][x].setLayoutX(x*64+10);
@@ -58,15 +64,25 @@ public class HelloController {
             public void handle(long l) {
                 if (l > ido) {
                     ido = l + 100_000_000;
+                    most = l;
                     angle += 36;
                     arrow.setRotate(angle);
+
+                    for (int y = 0; y < 10; y++) for (int x = 0; x < 15; x++) {
+                        if (intT[y][x] == OPEN && eletT[y][x] < ido) {
+                            eletT[y][x] = 0;
+                            lbT[y][x].setGraphic(new ImageView(semmi));
+                            intT[y][x] = NULL;
+
+                            feldolgDb++;
+                            feldolgLb.setText(feldolgDb + " db");
+                        }
+                    }
                 }
+
             }
         };
         timer.start();
-    }
-
-    private void onPalyaKatt(int y, int x) {
     }
 
     public void onClickRand() {
@@ -80,7 +96,26 @@ public class HelloController {
                     lbT[y][x].setGraphic(new ImageView(semmi));
                     intT[y][x] = NULL;
                 }
+                eletT[y][x] = 0;
+                feldolgDb = 0;
+                letettDb = 0;
+                feldolgLb.setText("0 db");
+                letettLb.setText("0 db");
             }
+        }
+    }
+
+    private void onPalyaKatt(int y, int x) {
+        if (intT[y][x] == CLOSE && (y == 0 || intT[y - 1][x] == NULL)) {
+            intT[y][x] = OPEN;
+            lbT[y][x].setGraphic(new ImageView(boxopen));
+            eletT[y][x] = most + 1_000_000_000;
+        } else if (intT[y][x] == NULL && (y < 9 && intT[y + 1][x] == CLOSE) || y == 9) {
+            intT[y][x] = CLOSE;
+            lbT[y][x].setGraphic(new ImageView(box));
+
+            letettDb++;
+            letettLb.setText(letettDb + " db");
         }
     }
 }
